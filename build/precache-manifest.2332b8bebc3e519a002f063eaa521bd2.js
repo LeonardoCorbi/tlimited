2f0119bfdea23bd5de7490b821d4ce76e261568c
LeonardoCorbi <leonardocorbi25@hotmail.com>
2020-09-13 15:01:18 -0300

    set build folder

---
+++ build/precache-manifest.2332b8bebc3e519a002f063eaa521bd2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40942ccc31c3c40d80358b1d11be074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd5207cc8f2d8872ed57",
+    "url": "/static/js/2.55dbe989.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.55dbe989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52891164adb0a89c56d4",
+    "url": "/static/js/main.b3cc336c.chunk.js"
+  },
+  {
+    "revision": "f2b9f9842c027cd8e26a",
+    "url": "/static/js/runtime-main.0c77ef25.js"
+  },
+  {
+    "revision": "be6a3a5d1fbc52ee56ff3123717d9d9f",
+    "url": "/static/media/adTodos.be6a3a5d.png"
+  },
+  {
+    "revision": "a04b28c52627e1d793d0df1e707668bc",
+    "url": "/static/media/designer1.a04b28c5.png"
+  },
+  {
+    "revision": "56774a41ae8d95245719cddeaba50ccf",
+    "url": "/static/media/flashIcon.56774a41.svg"
+  },
+  {
+    "revision": "343249144f06ff25f716b87d523b5f10",
+    "url": "/static/media/foot1.34324914.png"
+  },
+  {
+    "revision": "1325e3c414313931187c5bb8d210e0f0",
+    "url": "/static/media/nikeIcon.1325e3c4.svg"
+  },
+  {
+    "revision": "a9cbc7d2b67148476ff36e76d34867ab",
+    "url": "/static/media/shoe1.a9cbc7d2.png"
+  },
+  {
+    "revision": "2b868dc8d42b9fb155b7c3d3039b7ace",
+    "url": "/static/media/shoe2.2b868dc8.png"
+  },
+  {
+    "revision": "a19bdfaed5c3bc9602085c2eb2d513ab",
+    "url": "/static/media/shoe3.a19bdfae.png"
+  },
+  {
+    "revision": "73261141331733b1c9c31eae6ff47c9f",
+    "url": "/static/media/shoe4.73261141.png"
+  }
+]);
